b497526788b296041821a6111bbd177dfb75d751
Kayla Marie Sison <kaylamas@usc.edu>
2018-11-06 17:06:07 -0800

    servlets and js functions to grab stats

--- stats.js
+++ stats.js
@@ -8,20 +8,20 @@ function getNumOnlineUsers() {
 		success: function(response) {
 			document.getElementById("numOnlineUsers").innerHTML = response;
 		}
-	});  
+	});
 }
 
 // Display today's successful match rate
 function getSuccessRate() {
 	$.ajax({
-		url:"numOnlineUsersServlet",
+		url:"successRateServlet",
 		data:{
 		},
 		type:'post',
 		success: function(response) {
 			document.getElementById("successRate").innerHTML = response;
 		}
-	});  
+	});
 }
 
 // Immediately display statistics whenever homepage loads
