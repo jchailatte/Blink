c5aa36b51b5033388ec68a84916bbe5812cfbbc4
Xen1227 <xnegron@usc.edu>
2018-11-08 16:06:03 -0800

    fixed exception handling on survey page and survey servlet

--- sendSurveyPageData.java
+++ sendSurveyPageData.java
@@ -20,7 +20,18 @@ public class sendSurveyPageData extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		HttpSession session = request.getSession();
-		String userSignedIn = session.getAttribute("username").toString();
+		
+		String userSignedIn = "";
+		
+		if (session.getAttribute("username") != null)
+		{
+			userSignedIn = session.getAttribute("username").toString();
+		}
+		else
+		{
+			System.out.println("No user signed in");
+		}
+		
 		String name = request.getParameter("hiddenInput1");
 		String age = request.getParameter("hiddenInput2");
 		String gender = request.getParameter("hiddenInput3");
@@ -33,113 +44,116 @@ public class sendSurveyPageData extends HttpServlet {
 		String workout = request.getParameter("hiddenInput10");
 		String idealDate = request.getParameter("hiddenInput11");
 		
-		String success = "failed to send survey page data";
-		
 		Connection conn = null;
 		Statement st = null;
 		ResultSet rs = null;
 		PreparedStatement ps = null;
 		
-		try 
-		{
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost/Data?user=root&password=root&useSSL=false");
-			st = conn.createStatement();
-			rs = st.executeQuery("SELECT * from Survey where email='" + userSignedIn + "'");
-			while (rs.next()) 
-			{
-				ps = conn.prepareStatement("UPDATE Users SET name = ? WHERE email = ?");
-				ps.setString(1, name);
-				ps.setString(2, userSignedIn);
-				ps.executeUpdate();
-				
-				ps = conn.prepareStatement("UPDATE Users SET age = ? WHERE email = ?");
-				ps.setString(1, age);
-				ps.setString(2, userSignedIn);
-				ps.executeUpdate();
-				
-				ps = conn.prepareStatement("UPDATE Users SET gender = ? WHERE email = ?");
-				ps.setString(1, gender);
-				ps.setString(2, userSignedIn);
-				ps.executeUpdate();
-				
-				ps = conn.prepareStatement("UPDATE Users SET genderPreference = ? WHERE email = ?");
-				ps.setString(1, genderPreference);
-				ps.setString(2, userSignedIn);
-				ps.executeUpdate();
-				
-				ps = conn.prepareStatement("UPDATE Users SET indoorOutdoor = ? WHERE email = ?");
-				ps.setString(1, indoorOutdoor);
-				ps.setString(2, userSignedIn);
-				ps.executeUpdate();
-				
-				ps = conn.prepareStatement("UPDATE Users SET earlyBirdNightOwl = ? WHERE email = ?");
-				ps.setString(1, earlyBirdNightOwl);
-				ps.setString(2, userSignedIn);
-				ps.executeUpdate();
-				
-				ps = conn.prepareStatement("UPDATE Users SET catDog = ? WHERE email = ?");
-				ps.setString(1, catDog);
-				ps.setString(2, userSignedIn);
-				ps.executeUpdate();
-				
-				ps = conn.prepareStatement("UPDATE Users SET music = ? WHERE email = ?");
-				ps.setString(1, music);
-				ps.setString(2, userSignedIn);
-				ps.executeUpdate();
-				
-				ps = conn.prepareStatement("UPDATE Users SET sports = ? WHERE email = ?");
-				ps.setString(1, sports);
-				ps.setString(2, userSignedIn);
-				ps.executeUpdate();
-				
-				ps = conn.prepareStatement("UPDATE Users SET workout = ? WHERE email = ?");
-				ps.setString(1, workout);
-				ps.setString(2, userSignedIn);
-				ps.executeUpdate();
-				
-				ps = conn.prepareStatement("UPDATE Users SET idealDate = ? WHERE email = ?");
-				ps.setString(1, idealDate);
-				ps.setString(2, userSignedIn);
-				ps.executeUpdate();
-				
-				success = "successfully sent survey page data";
-			}
-			
-		}
-		catch (SQLException sqle) 
-		{
-			System.out.println (sqle.getMessage());
-		} 
-		catch (ClassNotFoundException cnfe) 
-		{
-			System.out.println (cnfe.getMessage());
-		} 
-		finally 
+		if (userSignedIn != "")
 		{
 			try 
 			{
-				if (rs != null) 
-				{ 
-					rs.close(); 
+				Class.forName("com.mysql.jdbc.Driver");
+				conn = DriverManager.getConnection("jdbc:mysql://localhost/Data?user=root&password=root&useSSL=false");
+				st = conn.createStatement();
+				rs = st.executeQuery("SELECT * from Survey where email='" + userSignedIn + "'");
+				while (rs.next()) 
+				{
+					ps = conn.prepareStatement("UPDATE Users SET name = ? WHERE email = ?");
+					ps.setString(1, name);
+					ps.setString(2, userSignedIn);
+					ps.executeUpdate();
+					
+					ps = conn.prepareStatement("UPDATE Users SET age = ? WHERE email = ?");
+					ps.setString(1, age);
+					ps.setString(2, userSignedIn);
+					ps.executeUpdate();
+					
+					ps = conn.prepareStatement("UPDATE Users SET gender = ? WHERE email = ?");
+					ps.setString(1, gender);
+					ps.setString(2, userSignedIn);
+					ps.executeUpdate();
+					
+					ps = conn.prepareStatement("UPDATE Users SET genderPreference = ? WHERE email = ?");
+					ps.setString(1, genderPreference);
+					ps.setString(2, userSignedIn);
+					ps.executeUpdate();
+					
+					ps = conn.prepareStatement("UPDATE Users SET indoorOutdoor = ? WHERE email = ?");
+					ps.setString(1, indoorOutdoor);
+					ps.setString(2, userSignedIn);
+					ps.executeUpdate();
+					
+					ps = conn.prepareStatement("UPDATE Users SET earlyBirdNightOwl = ? WHERE email = ?");
+					ps.setString(1, earlyBirdNightOwl);
+					ps.setString(2, userSignedIn);
+					ps.executeUpdate();
+					
+					ps = conn.prepareStatement("UPDATE Users SET catDog = ? WHERE email = ?");
+					ps.setString(1, catDog);
+					ps.setString(2, userSignedIn);
+					ps.executeUpdate();
+					
+					ps = conn.prepareStatement("UPDATE Users SET music = ? WHERE email = ?");
+					ps.setString(1, music);
+					ps.setString(2, userSignedIn);
+					ps.executeUpdate();
+					
+					ps = conn.prepareStatement("UPDATE Users SET sports = ? WHERE email = ?");
+					ps.setString(1, sports);
+					ps.setString(2, userSignedIn);
+					ps.executeUpdate();
+					
+					ps = conn.prepareStatement("UPDATE Users SET workout = ? WHERE email = ?");
+					ps.setString(1, workout);
+					ps.setString(2, userSignedIn);
+					ps.executeUpdate();
+					
+					ps = conn.prepareStatement("UPDATE Users SET idealDate = ? WHERE email = ?");
+					ps.setString(1, idealDate);
+					ps.setString(2, userSignedIn);
+					ps.executeUpdate();
 				}
-				if (st != null) 
-				{ 
-					st.close(); 
-				}
-				if (conn != null) 
-				{ 
-					conn.close(); 
-				}
-			} 
+				
+			}
 			catch (SQLException sqle) 
 			{
-				System.out.println(sqle.getMessage());
+				System.out.println (sqle.getMessage());
+			} 
+			catch (ClassNotFoundException cnfe) 
+			{
+				System.out.println (cnfe.getMessage());
+			} 
+			finally 
+			{
+				try 
+				{
+					if (rs != null) 
+					{ 
+						rs.close(); 
+					}
+					if (st != null) 
+					{ 
+						st.close(); 
+					}
+					if (conn != null) 
+					{ 
+						conn.close(); 
+					}
+				} 
+				catch (SQLException sqle) 
+				{
+					System.out.println(sqle.getMessage());
+				}
 			}
+		}
+		else
+		{
+			System.out.println("Database not updated because the user is not signed in");
 		}
 		
 		PrintWriter writer = response.getWriter();
-		writer.append(success);
+		writer.append(userSignedIn);
 		writer.flush();
 	}
 }
